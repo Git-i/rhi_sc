6e1db7009b1c73227e91ccf4ab9b224b5c25cf55
Eric Nwogbo <ericis2007@gmail.com>
2024-08-24 12:35:21 +0100

    Multiple file compilation

--- src/app.cpp
+++ src/app.cpp
@@ -1,8 +1,10 @@
 #include "RootSignature.h"
 #include "rhi_sc.h"
 #include <argparse/argparse.hpp>
+#include <memory>
 #include <ostream>
 #include <string>
+#include <ranges>
 #include <string_view>
 RHI::ShaderCompiler::OptimizationLevel GetOptimizationLevel(argparse::ArgumentParser& parser)
 {
@@ -48,14 +50,36 @@ RHI::ShaderStage GetShaderStage(argparse::ArgumentParser& parser)
     else
         return RHI::ShaderStage::None;
 }
+void AddMacroDefns(argparse::ArgumentParser& parser, std::unique_ptr<RHI::ShaderCompiler::CompileOptions>& args)
+{
+    auto macros = parser.get<std::vector<std::string>>("-D");
+    for(auto& macro : macros)
+    {
+        auto name = macro.find('=');
+        if(name == std::string::npos)
+        {
+            std::cout << "name = " << macro << std::endl;
+            args->AddMacroDefinition(macro, std::nullopt);
+        }
+        else {
+            auto mac_name = std::string_view(macro.data(), name);
+            auto mac_value = std::string_view(macro.data() + name + 1, macro.size() - name -1);
+            args->AddMacroDefinition(mac_name, mac_value);
+        }
+    }
+}
 int main(int argc, char** argv) 
 {
     argparse::ArgumentParser parser("rhi_sc");
     parser.add_description("Shader Compiler For Pistachio's RHI");
-    parser.add_argument("filename").help("The HLSL source file");
-    parser.add_argument("-o", "--output")
+    parser.add_argument("-i", "--inputs")
         .required()
-        .help("Output Filename (-o {FILENAME})");
+        .nargs(argparse::nargs_pattern::at_least_one)
+        .help("The HLSL source files (-i {FILENAMES})");
+    parser.add_argument("-o", "--outputs")
+        .required()
+        .nargs(argparse::nargs_pattern::at_least_one)
+        .help("Output Filename (-o {FILENAMES})");
     parser.add_argument("-t", "--target")
         .required()
         .help("Target Shader Stage (-t [pixel | vertex | compute | hull | domain | geometry ] - case insensitive)");
@@ -73,37 +97,33 @@ int main(int argc, char** argv)
     grp.add_argument("-OFast").flag().help("Same as -O3");
     parser.parse_args(argc, argv);
     auto args = RHI::ShaderCompiler::CompileOptions::New();
-    auto out_file = parser.get("-o");
     if (parser["-g"] == true)
     {
         args->EnableDebuggingSymbols();
     }
-    auto macros = parser.get<std::vector<std::string>>("-D");
-    for(auto& macro : macros)
-    {
-        auto name = macro.find('=');
-        if(name == std::string::npos)
-        {
-            std::cout << "name = " << macro << std::endl;
-            args->AddMacroDefinition(macro, std::nullopt);
-        }
-        else {
-            auto mac_name = std::string_view(macro.data(), name);
-            auto mac_value = std::string_view(macro.data() + name + 1, macro.size() - name -1);
-            args->AddMacroDefinition(mac_name, mac_value);
-        }
-    }
+    AddMacroDefns(parser, args);
     args->SetOptimizationLevel(GetOptimizationLevel(parser));
-    auto name = parser.get("filename");
-    auto cmp = RHI::ShaderCompiler::Compiler::New();
-    RHI::ShaderCompiler::ShaderSource src;
-    src.source = std::filesystem::path(name);
-    src.stage = GetShaderStage(parser);
-    auto res = cmp->CompileToFile(src, args, out_file);
-    if (res.error != RHI::ShaderCompiler::CompilationError::None)
+    auto out_files = parser.get<std::vector<std::string>>("-o");
+    auto names = parser.get<std::vector<std::string>>("-i");
+    if(names.size() != out_files.size())
     {
-        std::cerr << res.messages;
+        std::cerr << "Input and Output files must be the same length" << std::endl;
         return 1;
     }
+    size_t num_files = names.size();
+    auto cmp = RHI::ShaderCompiler::Compiler::New();
+    for(auto i : std::views::iota(static_cast<decltype(num_files)>(1), num_files))
+    {
+        RHI::ShaderCompiler::ShaderSource src;
+        src.source = std::filesystem::path(names[i]);
+        src.stage = GetShaderStage(parser);
+        auto res = cmp->CompileToFile(src, args, out_files[i]);
+        if (res.error != RHI::ShaderCompiler::CompilationError::None)
+        {
+            std::cerr << res.messages;
+            return 1;
+        }
+    }
+    
     return 0;
 }
